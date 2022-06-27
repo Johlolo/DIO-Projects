3c8619c106e2cad68da81ff429bd23329807c8c8
Johannes Pablo Rey <johannespablorey@gmail.com>
2022-06-27 13:34:17 -0300

    Added basic js module

---
+++ js-modules/main.mjs
@@ -0,0 +1,5 @@
+import { showAge, showCity, showHobby } from './functions.mjs';
+
+console.log(showAge("Johannes", 26));
+console.log(showCity("Johannes", "Joinville"));
+console.log(showHobby("Johannes", "travelling"));
