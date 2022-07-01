3bebb868249b00da22117732558c336ec5e7ab7f
Johannes Pablo Rey <johannespablorey@gmail.com>
2022-07-01 17:26:04 -0300

    Added Ifood React Counter

---
+++ React/states-effects/hooks-class/src/Components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Ifoodcounter from '../Ifoodcounter/Ifoodcounter';
+
+function App() {
+  return (
+    <>
+      <h1>Ifood Counter</h1>
+      <Ifoodcounter />
+    </>
+  );
+}
+
+export default App;
